3c6d83c98fb6f77748affce6acb2993f69bacb83
sheikhri <riyan.sheikh@synoverge.com>
2024-10-30 10:22:18 +0530

    groovy file

---
+++ vars/commonPipeline.groovy
@@ -0,0 +1,8 @@
+def call() {
+    echo "Running common steps..."
+    // Common steps here, for example:
+    checkout scm
+    sh 'echo Running unit tests'
+    sh 'echo Building the application'
+    // Add any other common logic here
+}
