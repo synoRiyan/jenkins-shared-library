979a597ce8a3e84b1214052dc575ca60c2815ddf
sheikhri <riyan.sheikh@synoverge.com>
2024-10-30 11:08:08 +0530

    dcoker opt code

---
+++ vars/dockerOperations.groovy
@@ -0,0 +1,31 @@
+// vars/dockerOperations.groovy
+def buildAndPushImage(String dockerRegistry, String imageName, String imageVersion) {
+    echo "Building Docker image..."
+    sh """
+        docker build -t ${dockerRegistry}/${imageName}:${imageVersion} .
+    """
+    
+    echo "Pushing Docker image..."
+    sh """
+        docker push ${dockerRegistry}/${imageName}:${imageVersion}
+    """
+}
+
+def copyFilesToServer(String serverUserName, String serverIp, String serverPassword, String deployPath) {
+    echo "Copying application files to the server..."
+    sh """
+        sshpass -p ${serverPassword} scp -o StrictHostKeyChecking=no -P 22 -r * ${serverUserName}@${serverIp}:${deployPath}
+    """
+}
+
+def deployWithDockerCompose(String serverUserName, String serverIp, String serverPassword, String dockerComposeFile, String deployPath) {
+    echo "Copying Docker Compose file to the server..."
+    sh """
+        sshpass -p ${serverPassword} scp -o StrictHostKeyChecking=no -P 22 ${dockerComposeFile} ${serverUserName}@${serverIp}:${deployPath}
+    """
+    
+    echo "Deploying Docker containers with Docker Compose..."
+    sh """
+        sshpass -p ${serverPassword} ssh -o StrictHostKeyChecking=no ${serverUserName}@${serverIp} 'cd ${deployPath} && docker-compose pull && docker-compose up -d'
+    """
+}
