1d94dd47552ef924c63967091eb723583d00828d
sheikhri <riyan.sheikh@synoverge.com>
2024-10-30 11:26:29 +0530

    asdf

--- vars/dockerOperations.groovy
+++ vars/dockerOperations.groovy
@@ -2,7 +2,7 @@
 def buildAndPushImage(String dockerRegistry, String imageName, String imageVersion) {
     echo "Building Docker image..."
     sh """
-        docker build -t ${dockerRegistry}/${imageName}:${imageVersion} .
+        docker build -t ${dockerRegistry}/${imageName}:${imageVersion} -f ./Dockerfile .
     """
     
     echo "Pushing Docker image..."
