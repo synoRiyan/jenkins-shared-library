063cb6fb8d48a6d561406134afd7ba513778d99f
sheikhri <riyan.sheikh@synoverge.com>
2024-11-04 14:14:09 +0530

    team notification

--- vars/commonPipeline.groovy
+++ vars/commonPipeline.groovy
@@ -6,3 +6,9 @@ def call() {
     sh 'echo Building the application'
     // Add any other common logic here
 }
+
+def sendTeamsNotification(String message) {
+    office365ConnectorSend message: message,
+        webhookUrl: 'https://synoverge.webhook.office.com/webhookb2/840c24c2-ce9f-479f-84be-33e0360b5af3@9afce432-20b4-4624-8400-1858002e5ae2/IncomingWebhook/0fe880ff23284ff3890c13519ca626b5/7e27b15c-b5c6-4e03-8118-3523b8984747/V2Yw-OhGJ3YQ4P5Ji2kz6FdqsURpWEfP0mOqxGFxQFJnw1'
+}
+
